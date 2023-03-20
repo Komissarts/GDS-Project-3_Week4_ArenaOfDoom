a09f883c22df5e2c29b645d335172068bb4e05b9
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-03-20 18:06:57 +1100

    PM1 (Starting)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    BoxCollider2D bc;
+    Rigidbody2D rb;
+    [SerializeField] int lives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Lvl1Enemy":
+            if(collision.gameObject.transform.position.y > this.transform.position.y)
+            {
+                Death(this);
+                if(lives > 0)
+                {
+                    lives = lives - 1;
+                }
+                else
+                {
+                    //Send to "Game Over" scene
+                }
+            }
+            else
+            {
+                Death(collision.gameObject);
+            }
+            break;
+        }
+    }
+
+    void Death(Object obj)
+    {
+        Destroy(obj);
     }
 }
