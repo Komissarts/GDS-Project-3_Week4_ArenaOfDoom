7c7aee19531fb3de7af93cff2805002d1427d975
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-03-21 16:55:14 +1100

    EN6 (Completed)

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,10 +7,25 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] GameObject enemyOrb;
     EnemyOrb enemyOrbClass;
 
+    float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch(this.gameObject.tag)
+        {
+            case "Lvl1Enemy":
+                moveSpeed = 6.0f;
+                break;
+            case "Lvl2Enemy":
+                moveSpeed = 8.0f;
+                break;
+            case "Lvl3Enemy":
+                moveSpeed = 10.0f;
+                break;
+            default:
+                break;
+        }
     }
 
     // Update is called once per frame
