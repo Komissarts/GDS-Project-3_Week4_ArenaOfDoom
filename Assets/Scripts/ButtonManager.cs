b3be99e594f016234baa5f816b6b8825a41fd713
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-03-21 14:04:32 +1100

    G1,2 (Completed)

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        EditorApplication.Exit(0);
+    }
+}
