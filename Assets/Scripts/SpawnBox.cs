5a8a0585bef2aa1cad02d9392bc45791f8e4c337
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-03-21 11:31:25 +1100

    EN1 (Started)

---
+++ Assets/Scripts/SpawnBox.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBox : MonoBehaviour
+{
+    [SerializeField] GameObject[] SpawnPoint;
+    [SerializeField] GameObject objToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy()
+    {
+        int index = Random.Range(0, (SpawnPoint.Length - 1));
+        Instantiate(objToSpawn, SpawnPoint[index].transform.position, Quaternion.identity);
+    }
+}
