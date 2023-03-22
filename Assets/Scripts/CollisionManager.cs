127918edcff87dbe2069392901b92145afad0c36
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-03-22 20:18:27 +1100

    Revert "LV4, PM3, 4 (Finally Completed)"
    This reverts commit 597d10b039bcf8c0d247e30e9ef2b763ff902683.

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -16,8 +16,7 @@ public class CollisionManager : MonoBehaviour
     int scoreMultiplier;
 
     EnemyMovement enemyMoveClass;
-    //[SerializeField] 
-    PlayerMovement playMoveClass;
+    [SerializeField] PlayerMovement playMoveClass;
 
     //get reference to movement script
     [SerializeField] float speedMultiplier = 1.5f;
@@ -29,7 +28,6 @@ public class CollisionManager : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
-        playMoveClass = GetComponent<PlayerMovement>();
     }
 
     void Start()
@@ -66,12 +64,10 @@ public class CollisionManager : MonoBehaviour
             case "GravPowerup":
                 playMoveClass.PlayerRB.gravityScale = playMoveClass.PlayerRB.gravityScale * gravMultiplier;
                 StartCoroutine(GravPowerup(powerupDuration));
-                collision.gameObject.SetActive(false);
                 break;
             case "SpeedPowerup":
                 playMoveClass.speed = playMoveClass.speed * speedMultiplier;
                 StartCoroutine(SpeedPowerup(powerupDuration));
-                collision.gameObject.SetActive(false);
                 break;
             default:
                 break;
@@ -140,6 +136,6 @@ public class CollisionManager : MonoBehaviour
     {
         yield return new WaitForSeconds(seconds);
 
-        playMoveClass.speed = playMoveClass.speed / speedMultiplier;
+        playMoveClass.speed = playMoveClass.speed * speedMultiplier;
     }
 }
