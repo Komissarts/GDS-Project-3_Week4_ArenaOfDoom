b3cf83e9ffdd5047dc26b8f238de2a1eb6df5d88
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-03-22 20:42:43 +1100

    LV4, PM3, 4 (Complete?)

---
+++ Assets/Scripts/SpawnPower.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPower : MonoBehaviour
+{
+    [SerializeField] GameObject[] SpawnPoint;
+    [SerializeField] GameObject[] powerToSpawn;
+    [SerializeField] float spawnTime = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnPowerup", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPowerup()
+    {
+        int spawnIndex = Random.Range(0, (SpawnPoint.Length));
+        int powerIndex = Random.Range(0, (SpawnPoint.Length));
+        Instantiate(powerToSpawn[powerIndex], SpawnPoint[spawnIndex].transform.position, Quaternion.identity);
+        AudioManager.Instance.PlaySFX("S9Spawn");
+    }
+}
